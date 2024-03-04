38f9b35cd497870aeef0c12d89bc2fad45edf0df
JinyongSon <wlsdydths@naver.com>
2024-03-04 17:22:56 +0900

    8일차

--- day08/Project1/Project1/VirtualDestructor.cpp
+++ day08/Project1/Project1/VirtualDestructor.cpp
@@ -37,6 +37,10 @@ public:
 
 int main(void)
 {
+	/*
+		상속관계에 있어서 객체포인터는 조심해야 한다.
+		부모타입의 객체 포인터로 자식 객체를 가리키는 경우(다형성) 조심
+	*/
 	First* ptr = new Second("simple", "complex");
 	delete ptr;
 	return 0;
