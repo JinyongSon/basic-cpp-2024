c5dcb099009a6eb7057460d697c0c3fd9f8f619b
JinyongSon <wlsdydths@naver.com>
2024-03-05 17:31:23 +0900

    9일차

---
+++ day09/Project1/Project1/test01.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+class Myclass {
+private:
+	int num;
+public:
+	Myclass(int anum = 0) : num(anum)
+	{}
+	void print() const 
+	{
+		cout << "value: " << num << endl;
+	}
+	Myclass operator+(Myclass &ref)
+	{
+		return Myclass(num + ref.num);
+	}
+
+};
+
+int main()
+{
+	Myclass a(10);
+	a.print();			// value: 10
+
+	Myclass b(a);
+	b.print();			// value: 10
+
+	Myclass c = b;
+	c.print();			// value: 10
+
+	Myclass d = a + b + c;
+	d.print();			// value: 30
+	return 0;
+}
---
+++ day09/Project1/Project1/test02.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+	int num1, num2;
+public:
+	Point(int anum1, int bnum2) : num1(anum1), num2(bnum2)
+	{}
+	void viewPoint()
+	{
+		cout << '[' << num1 << ", " << num2 << ']' << endl;
+	}
+	Point operator+(Point& ref)
+	{
+		Point pos(num1 + ref.num1, num2 + ref.num2);
+		return pos;
+	}
+	Point operator+(int num)
+	{
+		Point apos(num1 + num, num2 + num);
+		return apos;
+	}
+	friend Point operator+(int num, Point ref);
+};
+Point operator+(int num, Point ref)
+{
+	ref.num1 += num;
+	ref.num2 += num;
+	return ref;
+}
+
+namespace std
+{
+	class ostream
+	{
+	public:
+		ostream& operator<<(ostream& (*fp)(ostream& ostm))
+		{
+			return fp(*this);
+		}
+	};
+}
+
+int main()
+{
+	Point a(10, 20);
+	Point b(30, 40);
+	Point c = a + b;
+	Point d = a + 100;
+	Point e = 100 + a;
+
+	d.viewPoint();
+	e.viewPoint();
+
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	std::cout << e << std::endl;
+}
