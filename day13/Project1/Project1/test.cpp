649d0e652ee3f3d2f8b8dccbb02cf46bdd1d14d6
JinyongSon <wlsdydths@naver.com>
2024-03-14 09:12:28 +0900

    13일차

---
+++ day13/Project1/Project1/test.cpp
@@ -0,0 +1,35 @@
+﻿// while문과 if문만으로 배열 순서를 바꾸어 저장 출력하시오.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	char ary[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+	int n = sizeof(ary);
+	char reverseAry[sizeof(ary)];
+	int index = n - 1;
+	int currentIndex = 0;
+
+	while (currentIndex < n) {
+		reverseAry[index] = ary[currentIndex];
+		++currentIndex;
+		--index;
+	}
+
+	int i = 0;
+	while (true)
+	{
+		if (i >= 0 && i < n)
+		{
+			cout << reverseAry[i] << " ";
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
