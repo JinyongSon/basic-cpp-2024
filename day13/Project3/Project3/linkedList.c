961f62b57ac5e97b17b28dbd60826306a7a646b9
JinyongSon <wlsdydths@naver.com>
2024-05-14 09:33:41 +0900

    업데이트

---
+++ day13/Project3/Project3/linkedList.c
@@ -0,0 +1,216 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Book {
+    char title[100];
+    char author[100];
+    char publisher[100];
+    char summary[100];
+    int available;
+    struct Book* next;
+}book;
+
+void addBook(book* node)
+{
+    book* newNode = (book*)malloc(sizeof(book));
+
+    printf("책 제목을 입력하세요: ");
+    scanf("%s", newNode->title);
+    printf("책 저자를 입력하세요: ");
+    scanf("%s", newNode->author);
+    printf("책 출판사를 입력하세요: ");
+    scanf("%s", newNode->publisher);
+    printf("책 줄거리를 입력하세요: ");
+    scanf("%s", newNode->summary);
+    newNode->available = 1;
+    newNode->next = NULL;
+
+    while (node->next != NULL) {
+        node = node->next;
+    }
+    node->next = newNode;
+    printf("도서가 등록되었습니다\n");
+}
+
+void searchBook(book* node, char* name) {
+    char borrowyn[100];
+    char returnyn[100];
+    book* curr = node->next;
+    while (curr != NULL) {
+        if (strcmp(curr->title, name) != 0 && strcmp(curr->author, name) != 0 && strcmp(curr->publisher, name) != 0) {
+            curr = curr->next;
+            if (curr == NULL) {
+                printf("찾으려는 책이 없습니다\n");
+            }
+        }
+        else if (strcmp(curr->title, name) == 0 || strcmp(curr->author, name) == 0 || strcmp(curr->publisher, name) == 0) {
+            printf("제목: %s\n저자: %s\n출판사: %s\n줄거리: %s\n대여 가능 여부: %s\n",
+                curr->title, curr->author, curr->publisher,
+                curr->summary, curr->available ? "대여 가능" : "대여 중");
+            if (curr->available == 1) {
+                printf("대여 하시겠습니까?(y / n)");
+                scanf("%s", borrowyn);
+                while (getchar() != '\n');
+                if (strcmp(borrowyn, "y") == 0 && curr->available == 1) {
+                    curr->available = 0;
+                    printf("대여 완료되었습니다\n");
+                    break;
+                }
+                else if (strcmp(borrowyn, "y") == 0 && curr->available == 0) {
+                    printf("이미 대여 중 입니다\n");
+                    break;
+                }
+                else {
+                    printf("다시 입력하세요\n");
+                }
+            }
+            else if (curr->available == 0) {
+                printf("반납 하시겠습니까?(y / n)");
+                scanf("%s", returnyn);
+                while (getchar() != '\n');
+                if (strcmp(returnyn, "y") == 0 && curr->available == 0) {
+                    curr->available = 1;
+                    printf("반납 완료되었습니다\n");
+                    break;
+                }
+                else if (strcmp(returnyn, "n") == 0) {
+                    break;
+                }
+                else {
+                    printf("다시 입력하세요\n");
+                }
+            }
+            //break;
+        }
+    }
+}
+
+void deleteBook(book* node)
+{
+    book* curr = node;
+    book* prev = NULL;
+    char deltitle[100];
+    printf("삭제할 제목을 입력하세요: ");
+    scanf("%s", deltitle);
+
+    while (curr != NULL) {
+        if (strcmp(curr->title, deltitle) == 0) {
+            if (prev == NULL) {
+                node = curr->next;
+            }
+            else {
+                prev->next = curr->next;
+            }
+            printf("도서가 삭제되었습니다.\n");
+            free(curr);
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
+void showBook(book* node)
+{
+    int i = 1;
+    book* curr = node->next;
+    if (curr != NULL) {
+        printf("도서목록\n");
+    }
+    else if (curr == NULL) {
+        printf("등록된 도서가 없습니다\n");
+    }
+    while (curr != NULL) {
+        printf("%d. 제목: %s\n   저자: %s\n   출판사: %s\n   줄거리: %s\n   대여 가능 여부: %s\n"
+            , i, curr->title, curr->author, curr->publisher
+            , curr->summary, curr->available ? "대여 가능" : "대여 중");
+        i++;
+        curr = curr->next;
+    }
+}
+
+void allFreeNode(book* node)
+{
+    book* curr = node->next;
+    while (curr != NULL) {
+        node->next = curr->next;
+        free(curr);
+        curr = node->next;
+    }
+}
+
+void main()
+{
+    book* head = (book*)malloc(sizeof(book));
+    head->next = NULL;
+
+    while (true)
+    {
+        int num;
+        int searchnum;
+        char searchname[100];
+        printf("**** 메뉴 ****\n");
+        printf("1 도서등록\n");
+        printf("2 도서검색\n");
+        printf("3 도서삭제\n");
+        printf("4 도서출력\n");
+        printf("0 종료\n");
+        printf("메뉴를 선택하세요: ");
+        scanf("%d", &num);
+
+        while (getchar() != '\n');
+
+        if (num == 1) {
+            addBook(head);
+        }
+        else if (num == 2) {
+            while (true)
+            {
+                printf("검색 메뉴\n");
+                printf("1 제목\n");
+                printf("2 저자\n");
+                printf("3 출판사\n");
+                printf("메뉴를 선택하세요: ");
+                scanf("%d", &searchnum);
+                while (getchar() != '\n');
+                if (searchnum == 1) {
+                    printf("찾으려는 책 제목을 입력하세요: ");
+                    scanf("%s", searchname);
+                    searchBook(head, searchname);
+                    break;
+                }
+                else if (searchnum == 2) {
+                    printf("찾으려는 책 저자를 입력하세요: ");
+                    scanf("%s", searchname);
+                    searchBook(head, searchname);
+                    break;
+                }
+                else if (searchnum == 3) {
+                    printf("찾으려는 출판사를 입력하세요: ");
+                    scanf("%s", searchname);
+                    searchBook(head, searchname);
+                    break;
+                }
+                else {
+                    printf("다시 입력하세요\n");
+                }
+            }
+            //            searchBook(head);
+        }
+        else if (num == 3) {
+            deleteBook(head);
+        }
+        else if (num == 4) {
+            showBook(head);
+        }
+        else if (num == 0) {
+            printf("프로그램을 종료합니다\n");
+            allFreeNode(head);
+            break;
+        }
+        else {
+            printf("다시 입력하세요\n");
+        }
+    }
+}
---
+++ day13/Project3/Project3/test01.cpp
@@ -0,0 +1,211 @@
+﻿
+/*
+       File:ProductMange.c
+       Created: 24-03-06
+       Author: 손진용
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+// 부모 클래스 Product 정의
+class Product {
+public:
+    int id;
+    double price;
+    string producer;
+
+    // 생성자 정의
+    Product(int _id, double _price, const string& _producer) : id(_id), price(_price), producer(_producer) {}
+};
+
+// 자식 클래스 Book 정의
+class Book : public Product {
+public:
+    string ISBN;
+    string author;
+    string title;
+
+    // 생성자 정의
+    Book(int _id, double _price, const string& _producer, const string& _ISBN, const string& _author, const string& _title)
+        : Product(_id, _price, _producer), ISBN(_ISBN), author(_author), title(_title) {}
+};
+
+// 자식 클래스 Handphone 정의
+class Handphone : public Product {
+public:
+    string model;
+    int RAM;
+
+    // 생성자 정의
+    Handphone(int _id, double _price, const string& _producer, const string& _model, int _RAM)
+        : Product(_id, _price, _producer), model(_model), RAM(_RAM) {}
+};
+
+// 자식 클래스 Computer 정의
+class Computer : public Product {
+public:
+    string model;
+    string cpu;
+    int RAM;
+
+    // 생성자 정의
+    Computer(int _id, double _price, const string& _producer, const string& _model, const string& _cpu, int _RAM)
+        : Product(_id, _price, _producer), model(_model), cpu(_cpu), RAM(_RAM) {}
+};
+
+int main() {
+    Product* products[100] = { nullptr }; // 상품을 가리키는 포인터 배열 초기화
+
+    int choice;
+    do {
+        // 메인 화면 출력
+        cout << "------상품관리 프로그램-----" << endl;
+        cout << "1. 상품추가  2. 상품출력  3. 상품검색  0. 종료" << endl;
+        cout << "선택: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            int type;
+            cout << "1. 책  2. 핸드폰  3. 컴퓨터" << endl;
+            cout << "선택: ";
+            cin >> type;
+
+            int productId;
+            cout << "상품 ID 입력: ";
+            cin >> productId;
+
+            if (productId < 0 || productId >= 100) {
+                cout << "잘못된 상품 ID입니다." << endl;
+                break;
+            }
+
+            switch (type) {
+            case 1: {
+                // 책 추가 로직
+                double book_price;
+                string book_producer, book_ISBN, book_author, book_title;
+                cout << "책 정보 입력" << endl;
+                cout << "가격: ";
+                cin >> book_price;
+                cout << "제조사: ";
+                cin >> book_producer;
+                cout << "ISBN: ";
+                cin >> book_ISBN;
+                cout << "작가: ";
+                cin >> book_author;
+                cin.ignore(); // 이전 입력 버퍼 비우기
+                cout << "제목: ";
+                getline(cin, book_title); // 공백 포함한 문자열 입력 받기
+                products[productId] = new Book(productId, book_price, book_producer, book_ISBN, book_author, book_title);
+                break;
+            }
+            case 2: {
+                // 핸드폰 추가 로직
+                double handphone_price;
+                string handphone_producer, handphone_model;
+                int handphone_RAM;
+                cout << "핸드폰 정보 입력" << endl;
+                cout << "가격: ";
+                cin >> handphone_price;
+                cout << "제조사: ";
+                cin >> handphone_producer;
+                cout << "모델: ";
+                cin >> handphone_model;
+                cout << "RAM: ";
+                cin >> handphone_RAM;
+
+                if (productId < 0 || productId >= 100) {
+                    cout << "잘못된 상품 ID입니다." << endl;
+                    break;
+                }
+
+                products[productId] = new Handphone(productId, handphone_price, handphone_producer, handphone_model, handphone_RAM);
+                break;
+            }
+            case 3: {
+                // 컴퓨터 추가 로직
+                double computer_price;
+                string computer_producer, computer_model, computer_cpu;
+                int computer_RAM;
+                cout << "컴퓨터 정보 입력" << endl;
+                cout << "가격: ";
+                cin >> computer_price;
+                cout << "제조사: ";
+                cin >> computer_producer;
+                cout << "모델: ";
+                cin >> computer_model;
+                cout << "CPU: ";
+                cin >> computer_cpu;
+                cout << "RAM: ";
+                cin >> computer_RAM;
+
+                if (productId < 0 || productId >= 100) {
+                    cout << "잘못된 상품 ID입니다." << endl;
+                    break;
+                }
+
+                products[productId] = new Computer(productId, computer_price, computer_producer, computer_model, computer_cpu, computer_RAM);
+                break;
+            }
+            default:
+                cout << "잘못된 선택입니다." << endl;
+                break;
+            }
+            break;
+        }
+        case 2: {
+            // 상품 출력 로직
+            cout << "상품 목록 출력" << endl;
+            bool productsFound = false;
+            for (int i = 0; i < 100; ++i) {
+                if (products[i] != nullptr) {
+                    cout << "상품 ID: " << products[i]->id << endl;
+                    cout << "가격: " << products[i]->price << endl;
+                    cout << "제조사: " << products[i]->producer << endl;
+                    cout << endl;
+                    productsFound = true;
+                }
+            }
+            if (!productsFound) {
+                cout << "등록된 상품이 없습니다." << endl;
+            }
+            break;
+        }
+        case 3: {
+            // 상품 검색 로직
+            int searchID;
+            cout << "검색할 상품 ID 입력: ";
+            cin >> searchID;
+
+            if (searchID < 0 || searchID >= 100 || products[searchID] == nullptr) {
+                cout << "해당 ID의 상품이 없습니다." << endl;
+            }
+            else {
+                cout << "검색 결과" << endl;
+                cout << "상품 ID: " << products[searchID]->id << endl;
+                cout << "가격: " << products[searchID]->price << endl;
+                cout << "제조사: " << products[searchID]->producer << endl;
+                cout << endl;
+            }
+            break;
+        }
+        case 0:
+            cout << "프로그램을 종료합니다." << endl;
+            break;
+        default:
+            cout << "잘못된 선택입니다." << endl;
+            break;
+        }
+    } while (choice != 0);
+
+    // 동적으로 할당된 메모리 해제
+    for (int i = 0; i < 100; ++i) {
+        if (products[i] != nullptr) {
+            delete products[i];
+        }
+    }
+
+    return 0;
+}
